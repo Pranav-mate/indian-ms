dccb7409843c780d6a3743ff81b72f8d303873cc
Pranav-mate <pranavmate@gmail.com>
2023-03-07 06:25:49 +0530

    fix

--- src/main/java/com/example/indianms/train.java
+++ src/main/java/com/example/indianms/train.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class train {
     @GetMapping("/train")
     public String getData(){
-        return "book the train";
+        return "book the train from paytm";
     }
 }
